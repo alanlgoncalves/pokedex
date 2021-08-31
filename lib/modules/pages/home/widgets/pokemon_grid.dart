7a43e0bfa7b2fc9e7e73946df6d92529dde57652
alanlgoncalves <alan.lgoncalves@outlook.com>
2021-08-30 21:04:17 -0300

    refactor: Remove fade animation

--- lib/modules/pages/home/widgets/pokemon_grid.dart
+++ lib/modules/pages/home/widgets/pokemon_grid.dart
@@ -1,37 +1,21 @@
-import 'package:auto_animated/auto_animated.dart';
 import 'package:flutter/material.dart';
 import 'package:pokedex/modules/pages/home/widgets/poke_item.dart';
 import 'package:pokedex/modules/pages/pokemon_details/pokemon_details.dart';
 import 'package:pokedex/shared/stores/pokeapi_store.dart';
-import 'package:pokedex/shared/widgets/fade_animation.dart';
 
 class PokemonGridWidget extends StatelessWidget {
-  final _scrollController = ScrollController();
   final PokeApiStore pokeApiStore;
 
   PokemonGridWidget({Key? key, required this.pokeApiStore}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return LiveSliverGrid(
-      showItemDuration: const Duration(milliseconds: 50),
-      showItemInterval: const Duration(milliseconds: 10),
-      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        mainAxisSpacing: 10.0,
-        crossAxisSpacing: 10.0,
-        childAspectRatio: 3 / 2,
-      ),
-      itemBuilder: (
-        BuildContext context,
-        int index,
-        Animation<double> animation,
-      ) {
-        final _pokemon = pokeApiStore.getPokemon(index);
+    return SliverGrid(
+      delegate: SliverChildBuilderDelegate(
+        (context, index) {
+          final _pokemon = pokeApiStore.getPokemon(index);
 
-        return FadeTrasitionWidget(
-          animation: animation,
-          child: InkWell(
+          return InkWell(
             onTap: () async {
               await pokeApiStore.setPokemon(index);
 
@@ -48,13 +32,18 @@ class PokemonGridWidget extends StatelessWidget {
                 index: index,
               ),
             ),
-          ),
-        );
-      },
-      itemCount: pokeApiStore.pokemonsSummary != null
-          ? pokeApiStore.pokemonsSummary!.length
-          : 0,
-      controller: _scrollController,
+          );
+        },
+        childCount: pokeApiStore.pokemonsSummary != null
+            ? pokeApiStore.pokemonsSummary!.length
+            : 0,
+      ),
+      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        mainAxisSpacing: 10.0,
+        crossAxisSpacing: 10.0,
+        childAspectRatio: 3 / 2,
+      ),
     );
   }
 }
