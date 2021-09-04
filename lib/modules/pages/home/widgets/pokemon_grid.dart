01481f228a12cd9222e6ac2b8c2a79477c7f8cf7
alanlgoncalves <alan.lgoncalves@outlook.com>
2021-09-04 01:30:36 -0300

    Revert "feat: Test grid view without builder"
    This reverts commit bb8917e4

--- lib/modules/pages/home/widgets/pokemon_grid.dart
+++ lib/modules/pages/home/widgets/pokemon_grid.dart
@@ -1,52 +1,49 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:pokedex/modules/pages/home/widgets/poke_item.dart';
 import 'package:pokedex/modules/pages/pokemon_details/pokemon_details.dart';
 import 'package:pokedex/shared/stores/pokeapi_store.dart';
 
-class PokemonGridWidget extends StatefulWidget {
+class PokemonGridWidget extends StatelessWidget {
   final PokeApiStore pokeApiStore;
 
   PokemonGridWidget({Key? key, required this.pokeApiStore}) : super(key: key);
 
   @override
-  _PokemonGridWidgetState createState() => _PokemonGridWidgetState();
-}
-
-class _PokemonGridWidgetState extends State<PokemonGridWidget> {
-  @override
   Widget build(BuildContext context) {
-    return SliverGrid.extent(
-      maxCrossAxisExtent: 200,
-      mainAxisSpacing: 10.0,
-      crossAxisSpacing: 10.0,
-      childAspectRatio: 3 / 2,
-      children: [
-        if (widget.pokeApiStore.pokemonsSummary != null)
-          ...widget.pokeApiStore.pokemonsSummary!.map((it) {
-            final index = widget.pokeApiStore.pokemonsSummary!
-                .indexWhere((pokemon) => pokemon == it);
+    return SliverGrid(
+      delegate: SliverChildBuilderDelegate(
+        (context, index) {
+          final _pokemon = pokeApiStore.getPokemon(index);
 
-            return InkWell(
-              onTap: () async {
-                await widget.pokeApiStore.setPokemon(index);
+          return InkWell(
+            onTap: () async {
+              await pokeApiStore.setPokemon(index);
 
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (_) {
-                    return PokemonDetailsPage();
-                  }),
-                );
-              },
-              child: Ink(
-                child: PokeItemWidget(
-                  pokemon: widget.pokeApiStore.pokemonsSummary![index],
-                  index: index,
-                ),
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (_) {
+                  return PokemonDetailsPage();
+                }),
+              );
+            },
+            child: Ink(
+              child: PokeItemWidget(
+                pokemon: _pokemon,
+                index: index,
               ),
-            );
-          }).toList()
-      ],
+            ),
+          );
+        },
+        childCount: pokeApiStore.pokemonsSummary != null
+            ? pokeApiStore.pokemonsSummary!.length
+            : 0,
+      ),
+      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        mainAxisSpacing: 10.0,
+        crossAxisSpacing: 10.0,
+        childAspectRatio: 3 / 2,
+      ),
     );
   }
 }
