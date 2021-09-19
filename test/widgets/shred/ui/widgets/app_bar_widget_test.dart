32302d54014184d659d48ff88fc03e2d59c881f0
alanlgoncalves <alan.lgoncalves@outlook.com>
2021-09-18 23:51:04 -0300

    test: Appbar widget

---
+++ test/widgets/shred/ui/widgets/app_bar_widget_test.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:lottie/lottie.dart';
+import 'package:pokedex/shared/ui/widgets/animated_pokeball.dart';
+import 'package:pokedex/shared/ui/widgets/app_bar.dart';
+
+void main() {
+  testWidgets("Should render app bar without Lottie animation",
+      (WidgetTester tester) async {
+    await tester.pumpWidget(
+      new MediaQuery(
+        data: new MediaQueryData(),
+        child: new MaterialApp(
+          home: CustomScrollView(
+            slivers: [
+              AppBarWidget(title: "AppBar Title"),
+            ],
+          ),
+        ),
+      ),
+    );
+
+    expect(find.text("AppBar Title"), findsOneWidget);
+    expect(find.byType(AnimatedPokeballWidget), findsOneWidget);
+    expect(find.byIcon(Icons.menu), findsOneWidget);
+    expect(find.byType(Lottie), findsNothing);
+  });
+
+  testWidgets("Should render app bar with Lottie animation",
+      (WidgetTester tester) async {
+    await tester.pumpWidget(
+      new MediaQuery(
+        data: new MediaQueryData(),
+        child: new MaterialApp(
+          home: CustomScrollView(
+            slivers: [
+              AppBarWidget(
+                title: "AppBar Title",
+                lottiePath: "/assets",
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+
+    expect(find.text("AppBar Title"), findsOneWidget);
+    expect(find.byType(AnimatedPokeballWidget), findsOneWidget);
+    expect(find.byIcon(Icons.menu), findsOneWidget);
+    expect(find.byType(Lottie), findsOneWidget);
+  });
+}
